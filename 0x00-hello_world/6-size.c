29b6701f4947c45f9902ab292dc660df7e862c71
fawas iwayemi <iwayemiolawale@gmail.com>
2023-10-05 18:37:32 -0700

    hardest things

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print the size of various types
+ * return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("Size of char: %zu byte(s)\n", sizeof(char));
+	printf("Size of int: %zu byte(s)\n", sizeof(int));
+	printf("Size of float: %zu byte(s)\n", sizeof(float));
+	printf("Size of double: %zu byte(s)\n", sizeof(double));
+	printf("Size of long: %zu byte(s)\n", sizeof(long));
+	printf("Size of long long: %zu byte(s)\n", sizeof(long long));
+	return (0);
+}
