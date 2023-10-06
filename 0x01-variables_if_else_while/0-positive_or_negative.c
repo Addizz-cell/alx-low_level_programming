1db91ece8e29b54b0eaeb08a1ff420ee248d63c3
fawas iwayemi <iwayemiolawale@gmail.com>
2023-10-06 13:56:02 -0700

    hard things

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,20 +12,18 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = -RAND_MAX / 2;
-
-	printf("The number is: %d\n", n);
-	if (n > 0)
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
 	{
-		printf("is positive\n");
+		printf("%d is nagative\n", n);
 	}
-	else if (n == 0)
+	else if (n > 0)
 	{
-		printf("is zero\n");
+		printf("%d is possible\n", n);
 	}
 	else
 	{
-		printf("is negative\n");
+		printf("%d is zero\n", n);
 	}
 
 	return (0);
