9c0ed196ad5a241de837f12d3c4bc71b0658049e
fawas iwayemi <iwayemiolawale@gmail.com>
2023-10-07 09:19:37 -0700

    commited

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: should return 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
