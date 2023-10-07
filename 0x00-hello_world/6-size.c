155731cbcce580888a14b6688ecb01d3b58aab52
fawas iwayemi <iwayemiolawale@gmail.com>
2023-10-07 08:34:40 -0700

    corrected the betty error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,8 @@
 
 /**
  * main - print the size of various types,
- * return: 0 if exited properly, non-zero otherwise
+ *
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
