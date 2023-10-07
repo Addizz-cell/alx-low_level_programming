b36d0868f87c9fa57f6b1702011548007b4b0710
fawas iwayemi <iwayemiolawale@gmail.com>
2023-10-07 15:35:08 -0700

    easy

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,13 +7,16 @@
 
 int main(void)
 {
-	int i;
-
-	for (i = 0; i < 10; i++)
-	{
-		putchar(1 + '0');
-	}
-
+	putchar('0');
+	putchar('1');
+	putchar('2');
+	putchar('3');
+	putchar('4');
+	putchar('5');
+	putchar('6');
+	putchar('7');
+	putchar('8');
+	putchar('9');
 	putchar('\n');
 
 	return (0);
