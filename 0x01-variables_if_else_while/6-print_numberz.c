d147f916ad5ae8885bd1bebb72092fc37dede210
fawas iwayemi <iwayemiolawale@gmail.com>
2023-10-07 23:27:00 -0700

    printerall

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,16 +7,13 @@
 
 int main(void)
 {
-	putchar('0');
-	putchar('1');
-	putchar('2');
-	putchar('3');
-	putchar('4');
-	putchar('5');
-	putchar('6');
-	putchar('7');
-	putchar('8');
-	putchar('9');
+	int n = 0;
+
+	while (n <= 9)
+	{
+		putchar(n + '0');
+		++n;
+	}
 	putchar('\n');
 
 	return (0);
