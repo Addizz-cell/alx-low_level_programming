3f633b95450fe22da33711b0edd4ee88580cc2a0
fawas iwayemi <iwayemiolawale@gmail.com>
2023-10-11 11:55:54 -0700

    slow

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - Print the sign
+ * @n: The number to be chechked
+ *
+ * Return: 1 if n is greater than zero, 0 if n is * zero, -1 if less than 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('0');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+	else
+	{
+		putchar('0');
+		return (0);
+	}
+}
