d33049f45bc727b56a1de2ea2e1265f2d2c84f92
fawas iwayemi <iwayemiolawale@gmail.com>
2023-10-11 12:22:50 -0700

    fast

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,16 +12,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		putchar('+');
-		return (1);
+		return 1;
 	}
 	else if (n < 0)
 	{
 		putchar('-');
-		return (-1);
+		return -1;
 	}
 	else
 	{
 		putchar('0');
-		return (0);
+		return 0;
 	}
 }
