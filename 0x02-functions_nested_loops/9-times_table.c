2749f4d72215b7f4e7e46c99228ecce4a0f37a81
fawas iwayemi <iwayemiolawale@gmail.com>
2023-10-11 17:37:41 -0700

    i don die

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - code goes her for the function
+ *
+ */
+void times_table(void)
+{
+	int i = 0;
+
+	while (i <= 10)
+	{
+		printf("9 x %d = %d\n", i, 9 * i);
+		i++;
+	}
+}
+
