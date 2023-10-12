35c531a38f10a1048e427f8eadb659e27e4bc6ef
fawas iwayemi <iwayemiolawale@gmail.com>
2023-10-12 09:53:25 -0700

    good job

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -11,7 +11,7 @@ void positive_or_negative(int i)
 	if (i < 0)
 		printf("%d is negative", i);
 	else if (i == 0)
-		printf("%d is zero",i);
+		printf("%d is zero", i);
 	else
 		printf("%d is positive", i);
 }
