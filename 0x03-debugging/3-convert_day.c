4972adb09b556e4b32f6b46662bdfa9b64e2ab9b
fawas iwayemi <iwayemiolawale@gmail.com>
2023-10-13 16:33:28 -0700

    updated file

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -2,50 +2,51 @@
 
 /**
  * convert_day - converts day of month to day of year, without accounting
- * main - Entry point 
  * for leap year
  * @month: month in number format
  * @day: day of month
  * Return: day of year
  */
 
-int convert_day(int month, int day);
-
-int main(void)
-{
-	int month = 4;
-	int day = 15;
-
-	int day_of_year = convert_day(month, day);
-
-
-	printf("The day of the year is: %d\n", day_of_year);
-
-	return (0);
-}
-
 int convert_day(int month, int day)
 {
-	int day_of_year = 0;
-
 	switch (month)
 	{
-		case 1:
-			day_of_year = day;
-			break;
-		case 2:
-			day_of_year = 31 + day;
-			break;
-		case 3:
-			day_of_year = 31 + 28 + day;
-			break;
-		case 4:
-			day_of_year = 31 + 28 + 31 + day;
-			break;
-
-		default:
-			printf("Invalid month\n");
-			break;
+	case 2:
+		day = 31 + day;
+		break;
+	case 3:
+		day = 59 + day;
+		break;
+	case 4:
+		day = 90 + day;
+		break;
+	case 5:
+		day = 120 + day;
+		break;
+	case 6:
+		day = 151 + day;
+		break;
+	case 7:
+		day = 181 + day;
+		break;
+	case 8:
+		day = 212 + day;
+		break;
+	case 9:
+		day = 243 + day;
+		break;
+	case 10:
+		day = 273 + day;
+		break;
+	case 11:
+		day = 304 + day;
+		break;
+	case 12:
+		day = 334 + day;
+		break;
+	default:
+		break;
 	}
-	return (day_of_year);
-}
+	return (day);
+}	
