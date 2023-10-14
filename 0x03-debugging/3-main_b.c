b2d887f2d8ef14fcde11703e39a61232695ca94b
fawas iwayemi <iwayemiolawale@gmail.com>
2023-10-13 17:35:46 -0700

    All file now is updated

---
+++ 0x03-debugging/3-main_b.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main()
+{
+	int month = 10;
+	int day = 13;
+	int year = 2023;
+
+	print_remaining_days(month, day, year);;
+
+	return (0);
+}
