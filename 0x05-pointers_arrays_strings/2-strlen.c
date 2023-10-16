c2dab7b533167dd397927b8b402f42802a5b2b46
fawas iwayemi <iwayemiolawale@gmail.com>
2023-10-16 08:23:07 -0700

    updated task 1, 2, 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string to calculate the length of.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}
