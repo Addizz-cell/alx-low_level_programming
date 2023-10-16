5ab26b4c545aa513a4073d03f9908ef415ba0478
fawas iwayemi <iwayemiolawale@gmail.com>
2023-10-16 08:01:45 -0700

    First commit after task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value of an integer to 98.
+ * @n: Pointer to the integer to be updated.
+ */
+
+void reset_to_98(int *n)
+{
+	*n =  98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Welcome to the pointers and array directory
