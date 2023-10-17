d271ada3f07ed6e3b0b511046527280f48efa5e9
fawas iwayemi <iwayemiolawale@gmail.com>
2023-10-16 17:24:59 -0700

    commited files

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,15 +11,26 @@ int _atoi(char *s)
 	int sign = 1;
 	int result = 0;
 	int i = 0;
+	int found = 0;
 
 	while (s[i] == '-' || s[i] == '+') {
-		if (s[i] == '-')
+		if (s[i] == '-') {
 			sign *= -1;
+		} else if (s[i] == '+') {
+			sign *= 1;
+		}
 		i++;
 	}
 
-	while (s[i] >= '0' && s[i] <= '9') {
-		result = result * 10 + (s[i] - '0');
+	while (s[i]) {
+		if (s[i] >= '0' && s[i] <= '9') {
+			result = result * 10 + (s[i] - '0');
+			found = 1;
+		} else {
+			if (found) {
+				break;
+			}
+		}
 		i++;
 	}
 
