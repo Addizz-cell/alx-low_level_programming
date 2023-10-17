26f42b04552c23fc17436b18d2fc41e6dcc032d3
fawas iwayemi <iwayemiolawale@gmail.com>
2023-10-16 17:53:36 -0700

    done

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,12 +8,13 @@ void puts_half(char *str)
 {
 	int length = 0;
 	int i;
+	int start;
 
 	while (str[length] != '\0') {
 		length++;
 	}
 
-	int start = (length - 1) / 2;
+	start = (length - 1) / 2;
 
 	for (i = start + 1; i < length; i++) {
 		_putchar(str[i]);
