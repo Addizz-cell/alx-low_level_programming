2a02972875367a7c837ae85d7165dfcf94363ef7
fawas iwayemi <iwayemiolawale@gmail.com>
2023-10-16 17:35:22 -0700

    commited files

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,38 +1,26 @@
 #include "main.h"
 
 /**
- * _atoi - function that converts a string to an integer.
- * @s: Iterate through the string and convert digits to integer
- * Return: result * sign
- */
+ * _atoi - converts a string to an integer
+ *
+ * @s: string input parameter
+ *
+ * Return: converted integer from string
+*/
 
 int _atoi(char *s)
 {
+	unsigned int num = 0;
 	int sign = 1;
-	int result = 0;
-	int i = 0;
-	int found = 0;
 
-	while (s[i] == '-' || s[i] == '+') {
-		if (s[i] == '-') {
+	do {
+		if (*s == '-')
 			sign *= -1;
-		} else if (s[i] == '+') {
-			sign *= 1;
-		}
-		i++;
-	}
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
 
-	while (s[i]) {
-		if (s[i] >= '0' && s[i] <= '9') {
-			result = result * 10 + (s[i] - '0');
-			found = 1;
-		} else {
-			if (found) {
-				break;
-			}
-		}
-		i++;
-	}
-
-	return result * sign;
+	return (num * sign);
 }
