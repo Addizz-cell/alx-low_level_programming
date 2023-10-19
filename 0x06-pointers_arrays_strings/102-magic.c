431880c21674b382b9f922f8db74c5830f80f101
fawas iwayemi <iwayemiolawale@gmail.com>
2023-10-19 10:07:25 -0700

    task 102

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - check your code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	p[1] = a[2] - 926;
+
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
