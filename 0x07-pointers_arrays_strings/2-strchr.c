f14277e924695164f425a5856f59ddb1b0447e21
fawas iwayemi <iwayemiolawale@gmail.com>
2023-10-23 10:56:33 -0700

    task 1-4

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,12 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s)
-	{
-		if (*s == c)
-			return (s);
-		s++;
-	}
+	int i = 0;
 
-	if (c == '\0')
-		return (s);
-	return (NULL);
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
 }
