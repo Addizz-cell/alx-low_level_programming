50b00ce79d750a4a5ec28cfcf4f0f114b640961b
fawas iwayemi <iwayemiolawale@gmail.com>
2023-10-25 13:51:20 -0700

    task 2 file updated

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,7 +9,7 @@
 int _strlen_recursion(char *s)
 {
 	if (*s)
-		return (1 + _strlen_recursion(s + 1)); /* Recursive call with incremented pointer */
+		return (1 + _strlen_recursion(s + 1));
 	else
 		return (0);
 }
