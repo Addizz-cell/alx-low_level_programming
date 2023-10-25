eaf94ee5ce6ff8a86a7334c22da0cddc5311a410
fawas iwayemi <iwayemiolawale@gmail.com>
2023-10-25 12:37:49 -0700

    first commit

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes a character to stdout
+ * @c: The character to print
+ * Return: On success 1, on error -1
+ */
+int _putchar(char c)
+{
+	return write(1, &c, 1);
+}
