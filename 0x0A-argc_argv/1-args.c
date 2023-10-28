e017d7ebf5d61b79cc544212ca3ea2c45382da07
fawas iwayemi <iwayemiolawale@gmail.com>
2023-10-28 07:15:09 -0700

    the first task code that will print the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include  <stdio.h>
+/**
+ * main - Entry point of the code
+ * @argc: number of arguments passed into it
+ * @argv: strings containing the command-line arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
