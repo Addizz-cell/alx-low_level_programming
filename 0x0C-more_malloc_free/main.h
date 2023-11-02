2801f9bfe5aeb771db963f56fa8a57ba986e2401
fawas iwayemi <iwayemiolawale@gmail.com>
2023-11-02 09:37:17 -0700

    last task done

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,6 +3,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void errors(void);
-int _strlen(char *s);
-int is_digit(char *s);
+int main(int argc, char *argv[]);
+void init(char *str, int l);
+int check_for_digits(char **av);
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+void _print(char *str, int l)
