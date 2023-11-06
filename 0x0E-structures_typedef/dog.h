2dc8d9ad99cc482a74224caabc667aac97102678
fawas iwayemi <iwayemiolawale@gmail.com>
2023-11-06 12:34:25 -0800

    def

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -24,6 +24,7 @@ typedef struct dog dog_t;
  * @d: Pointer to the struct dog
  */
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
