2ecd5ba7007665e7adce21ec9584819659c6d0f5
fawas iwayemi <iwayemiolawale@gmail.com>
2023-11-16 12:11:17 -0800

    compleated

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - Frees a list_t list
+ * @head: Pointer to the head of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
