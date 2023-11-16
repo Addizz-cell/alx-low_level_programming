3f0df5961b3009cb69f74a765880e3b448b15ea4
fawas iwayemi <iwayemiolawale@gmail.com>
2023-11-16 12:38:20 -0800

    done

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,18 @@
+section .data
+    hello db 'Hello, Holberton', 0
+    format db '%s', 10, 0
+
+section .text
+    global _start
+
+_start:
+    ; Prepare arguments to call printf
+    mov rdi, format   ; Format string
+    mov rsi, hello    ; Address of the string to print
+    xor rax, rax      ; Clear RAX to indicate printf from the PLT
+    call printf       ; Call printf function from the PLT
+
+    ; Exit
+    mov eax, 60       ; Exit syscall number
+    xor edi, edi      ; Exit status 0
+    syscall           ; Invoke syscall
