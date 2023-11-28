6e248e2d154e25742b732ef88a84b60dbc6bd304
fawas iwayemi <iwayemiolawale@gmail.com>
2023-11-27 17:25:26 -0800

    done

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,6 +10,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void close_files(int file1, int file2);
+char *create_buffer(char *file);
+void close_file(int fd);
 
 #endif /* MAIN_H */
